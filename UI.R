ef8632304a50d2f46029e896bc8fa76421728a68
Xiyao <xlong3@uic.edu>
2015-11-11 00:08:32 -0600

    First commit

---
+++ UI.R
@@ -0,0 +1,22 @@
+ library(shiny)
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("Hello Shiny!"),
+
+  # Sidebar with a slider input for the number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+))
